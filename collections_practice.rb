48d6a7c8f1f82b591a5b1839a0ee864e3aaab459
Morgan Fudge <mfudge92@gmail.com>
2017-11-01 16:16:07 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,8 +11,8 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  temp = array[3]
-  array[3] = array[2]
-  array[2] = temp
+  temp = array[2]
+  array[2] = array[1]
+  array[1] = temp
   return array
 end
